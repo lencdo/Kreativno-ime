b67c95709cee6ddfbfb34edfed7c230b6fcdedce
Anchiqua <anja.leskovsek@gmail.com>
2018-05-10 12:37:11 +0200

    popravljeno

--- podatki.py
+++ podatki.py
@@ -17,7 +17,7 @@ def shrani_text(text, directory, filename):
     return None
 
 
-#osnovni podatki
+#osnovni podatki - shranimo html spletne strani
 def download_url(url):
     datoteka  = requests.get(url)
     datoteka_text = datoteka.text
@@ -26,7 +26,11 @@ def download_url(url):
 
     return None
 
+#da dobimo imenik faks pokličemo naslednjo funkcijo
+#download_url(faks_url)
+
 #vzorec osnovnih podatkov
+#dodamo del spletne strani, ki nam bo v pomoč pri iskanju podrobnejših podatkov
 osnovni_vzorec = re.compile(
    " <span class=\"bld\">\d\.\d "
    "(?P<Naziv>.*)"
@@ -40,9 +44,18 @@ osnovni_vzorec = re.compile(
     )
 
 
-#vzorec_smeri = re.compile()
+#vzorec za smeri
+vzorec_smeri = re.compile(
+    "<td class=\"ac\">\d+<\/td>\s*<td>.*<span class=\"bld\">"
+    "(?P<Smer>.*)"
+    "<\/span> \("
+    "(?P<Stopnja>\w*)"
+    "\)<\/a>"
+    )
 
 
+#iz imenika pregleda vsako datoteko (html) in izlušči z vzorcem podatke,
+#ki jih pretvori v seznam slovarjev
 def seznam_iz_podatkov(imenik, vzorec):
     a=0
     sez=[]
@@ -57,8 +70,12 @@ def seznam_iz_podatkov(imenik, vzorec):
         print(a)
     return sez
 
+#glavni seznam slovarjev fakultet 
 seznam_podatkov = seznam_iz_podatkov('faks', osnovni_vzorec)
 
+
+#podobno kot pri download_url shrani html datoteko za vsak faks posebej,
+#iz katerih bomo v nadaljevanju pridobili smeri
 def download_faksi(podatki):
     dolzina = len(podatki)
     for i in range(0, dolzina):
@@ -71,32 +88,71 @@ def download_faksi(podatki):
 
     return None
 
+#da dobimo imenik univerze kličemo naslednjo funkcijo
+#download_faksi(seznam_podatkov)
+
+#podobna koda kot seznam_iz_podatkov, le da doda še naziv, da bomo lahko
+#kasneje smeri združili s podatki univerze po ključu Naziv
+def smeri_iz_podatkov(imenik, vzorec):
+    a=0
+    sez=[]
+    for ime_datoteke in os.listdir(imenik):
+        pot = os.path.join(imenik, ime_datoteke)
+        with open(pot, encoding = 'utf-8') as datoteka:
+            vsebina = datoteka.read()
+        for ujemanje in vzorec.finditer(vsebina):
+            podatki = ujemanje.groupdict()
+            podatki["Naziv"] = str(ime_datoteke)[:-5]
+            sez.append(podatki)
+            a+=1
+        print(a)
+    return sez
+
+seznam_smeri = smeri_iz_podatkov("univerze", vzorec_smeri)
 
 
-def sestavi_slovarje(seznam1, seznam2):
-    dolzina = len(seznam1)
+
+#funkcija, ki nam bo zlila skupaj oba seznama slovarjev
+def zlij_skupaj(seznam1, seznam2, ključ):
+    for item1 in seznam1:
+        for item2 in seznam2:
+            if item1[ključ] == item2[ključ]:
+                item1.update(item2)
+            else:
+                pass
+    return(seznam1)
+
+seznam1 = zlij_skupaj(seznam_smeri, seznam_podatkov, "Naziv")
+
+
+def izbrisi(seznam, ključ):
+    dolzina = len(seznam)
     for i in range(0, dolzina):
-        seznam1[i].update(seznam2[i])
-    return seznam1
+        seznam[i].pop(ključ)
+    return(seznam)
+
+#ker je ključ Spletna_stran odveč, ga odstranimo
+seznam2 = izbrisi(seznam1, "Spletna_stran")
+
+def ID(seznam):
+    dolzina = len(seznam)
+    for i in range(0, dolzina):
+        seznam[i]["ID"] = i
+    return(seznam)
 
 
+seznam = ID(seznam2)
 
 
-#funkcija, ki bo spojila skupaj dva seznama slovarjev, ki imata enak ključ
-def merge_lists(slovar1, slovar2, key):
-    merged = {}
-    for item in slovar1+slovar2:
-        if item[key] in merged:
-            merged[item[key]].update(item)
-        else:
-            merged[item[key]] = item
-    return [val for (_, val) in merged.items()]
-
-
-
-
-
-imena = []
+imena = [
+    "ID",
+    "Naziv",
+    "Ulica",
+    "Kraj",
+    "Postna_stevilka",
+    "Smer",
+    "Stopnja"
+    ]
     
 
 def zapisi_csv(podatki, polja, ime_datoteke):
