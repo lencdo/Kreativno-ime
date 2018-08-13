b8e20c1f03b7cdda48750365a81f2f675544dca2
Anchiqua <anja.leskovsek@gmail.com>
2018-08-13 20:47:37 +0200

    prosta dela

--- servis.py
+++ servis.py
@@ -179,4 +179,4 @@ def static(filename):
     return static_file(filename, root='assets')
 
 # poženemo strežnik na portu 8080, glej http://localhost:8080/
-run(host='localhost', port=8082)
+run(host='localhost', port=8080)
